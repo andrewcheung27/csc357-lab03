334d79ec5f801fb72d33f6de6b9a239d48b83a9c
andrewcheung27 <andrewcheung27@gmail.com>
2022-04-23 17:24:06 -0700

    doing the main thing

--- htable.c
+++ htable.c
@@ -1,71 +1,77 @@
+#include "htree.h"
+#include "list.h"
+
+#define NUM_CHARS 256
+
+
+void countChars(FILE *file, int *chars) {
+    int nextChar;
+
+    while ((nextChar = fgetc(file)) != EOF) {
+        chars[nextChar]++;
+    }
+}
+
+
 int main(int argc, char *argv[]) {
-    HashTable *ht = htCreate(DEFAULT_HT_SIZE);
+    int option;
+    int i;
     FILE *file;
 
-    long int wordsToCount = DEFAULT_WORDS_TO_COUNT;
-    const char *str;
-    char *endptr;
-    int option;
+    int *charHistogram;
+    HNode *hnode;
+    ListNode *hlist;
 
-    /* use getopt to parse args */
-    while ((option = getopt(argc, argv, ":n:")) != -1) {
-        switch (option) {
-            case 'n':
-                str = copyString(optarg);
-                if (str == NULL) {
-                    perror("malloc");
-                    exit(EXIT_FAILURE);
-                }
-
-                wordsToCount = strtol(str, &endptr, 0);
-                if (str == endptr) {  /* checks if strtol worked */
-                    fprintf(stderr,
-                            "Invalid k value (%s).\nusage: fw [-n num] [ file1 [ file 2 ...]\n",
-                            optarg);
-                    exit(EXIT_FAILURE);
-                }
-                free((char *) str);
-                break;
-
-            case '?':
-                fprintf(stderr,
-                        "Unknown option.\nusage: fw [-n num] [ file1 [ file 2 ...]\n");
-                exit(EXIT_FAILURE);
-
-            case ':':
-                fprintf(stderr,
-                        "Missing argument.\nusage: fw [-n num] [ file1 [ file 2 ...]\n");
-                exit(EXIT_FAILURE);
+    /* use getopt to make sure there are no options */
+    while ((option = getopt(argc, argv, ":")) != -1) {
+        if (option == '?') {
+            fprintf(stderr, 
+                    "No options accepted.\nusage: htable filename\n");
+            exit(EXIT_FAILURE);
         }
     }
 
-    /* read from stdin if there are no files provided */
+    /* make sure a filename is provided */
     if (optind == argc) {
-        ht = readFile(stdin, ht);
+        fprintf(stderr, "Filename required.\nusage: htable filename\n");
+        exit(EXIT_FAILURE);
     }
 
-    /* get args that aren't options (should be file names), read files,
-     * and put all words in the hash table */
-    while (optind < argc) {
-        file = fopen(argv[optind], "r");
-        if (file == NULL) {
-            fprintf(stderr,
-                    "%s: No such file or directory\n", argv[optind]);
-            optind++;
-            continue;
+    /* get filename from args and open file */
+    file = fopen(argv[optind++], "r");
+    if (file == NULL) {
+        fprintf(stderr, 
+                "%s: No such file or directory\nusage: htable filename\n", 
+                argv[optind]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* charHistogram is used to count how many of each character appear
+     * in the file. charHistogram[character] contains its frequency */
+    charHistogram = (int *) malloc(sizeof(int) * NUM_CHARS);
+    if (charHistogram == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    /* initialize histogram values to 0 */
+    for (i = 0; i < NUM_CHARS; i++) {
+        charHistogram[i] = 0;
+    }
+
+    /* count characters, insert a huffman tree for each unique character
+     * into the sorted list */
+    countChars(file, charHistogram);
+    for (i = 0; i < NUM_CHARS; i++) {
+        if (charHistogram[i] > 0) {
+            hnode = htreeInsert(NULL, charHistogram[i], i);
+            listInsert(hlist, hnode);
         }
-        ht = readFile(file, ht);
-        fclose(file);
-        optind++;
     }
 
-    /* print the stuff that is to be printed */
-    printf("The top %ld words (out of %d) are: \n",
-           wordsToCount, ht->numElements);
-    printMostCommonWords(ht, wordsToCount);
+    while (hlist != NULL) {
+        /* remove from hlist to get left and right, make new parent of
+         * left and right */
 
-    /* cleanup */
-    htDestroy(ht);
     return 0;
 }
 
